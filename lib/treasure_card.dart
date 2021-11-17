34fd7190358a6c1d82f37c0ab1fb396e2cad045a
Monty Rasmussen <montyr75@gmail.com>
2021-11-17 14:11:38 -0700

    added TreasureCard class

---
+++ lib/treasure_card.dart
@@ -0,0 +1,11 @@
+class TreasureCard {
+  final String  title;
+  final String description;
+  final bool isGood;
+
+  const TreasureCard({
+    required this.title,
+    required this.description,
+    required this.isGood,
+  });
+}
